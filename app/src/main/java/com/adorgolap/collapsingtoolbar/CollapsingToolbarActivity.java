8dee0f46b19b484793e21c9ec13308c3cb59b9a6
adorgolap <adorgolap@gmail.com>
2016-11-25 18:27:07 +0600

    experiment

---
+++ app/src/main/java/com/adorgolap/collapsingtoolbar/CollapsingToolbarActivity.java
@@ -0,0 +1,23 @@
+package com.adorgolap.collapsingtoolbar;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ifta on 11/25/16.
+ */
+
+public class CollapsingToolbarActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+
+//        CollapsingToolbarLayout collapsingToolbarLayout =
+//                (CollapsingToolbarLayout) findViewById(R.id.main_collapsing);
+//
+//        collapsingToolbarLayout.setTitle(getString(R.string.app_name));
+    }
+}
